f0331db2e7708755970616cb66a95108710dc2a1
amrendra1998 <Amrendra1999official@gmail.com>
2025-10-18 07:53:54 +0530

    file commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -9,16 +9,23 @@ function App() {
 
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <img src="" alt="" srcset="" />
-    <div className="container">
-      <h1>hello {name}</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste animi error tempore deserunt quis tempora, deleniti reprehenderit quidem, beatae molestiae ea! Provident excepturi, cupiditate autem cumque unde voluptates perspiciatis quos, ducimus totam odio labore.</p>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Textutils</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
     </>
   )
 }
