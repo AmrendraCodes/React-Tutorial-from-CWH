20a4fa30241e004e9fb1632649a143f5bef3bcd6
amrendra1998 <Amrendra1999official@gmail.com>
2025-10-18 07:08:23 +0530

    file commit message

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,13 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+let name = " Amrendra Kumar"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>This is A Rect App Enviornment</h1>
-      <p>Hello my name is Amrendra Kumar</p>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <img src="" alt="" srcset="" />
+    <div className="container">
+      <h1>hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste animi error tempore deserunt quis tempora, deleniti reprehenderit quidem, beatae molestiae ea! Provident excepturi, cupiditate autem cumque unde voluptates perspiciatis quos, ducimus totam odio labore.</p>
+    </div>
     </>
   )
 }
